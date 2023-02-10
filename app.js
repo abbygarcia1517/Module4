bbbb9cddd2a91d47f27c7ac1d7869da950f7467d
abbygarcia1517 <89549227+abbygarcia1517@users.noreply.github.com>
2023-02-10 17:40:54 -0600

    update

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const testscore = 80;
 
-if (testscore > 80) {
+if (testscore === 85) {
     console.log("Passed.");
-} else if (testscore < 70) {
- console.log("10 points from passing.");
 } else {
     console.log("Failed.");
 }
