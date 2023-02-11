7139be8c15e1533ccfecf5f2cd58ab6d85a69fc9
abbygarcia1517 <89549227+abbygarcia1517@users.noreply.github.com>
2023-02-10 18:17:13 -0600

    cchhh

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ if (englishcore < 85) {
 console.log("Science Test Date " + "October 15");
 
 const date = "October 20";
-console.log("Math Test Date is ${date}");
+const time = "3:00 P.M."
+console.log(
+    "Your test is scheduled for " + date + " at " + time + "Thank you.");
 
 
 
